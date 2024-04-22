9ca19827ee63d0adcabe6fc951146ff1e341725e
Terhide Imenger <166405679+kinglampson@users.noreply.github.com>
2024-04-22 12:06:50 +0800

    Add files via upload

---
+++ cat_lady.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>See the number of cats we have here</h2>
+<p>shows you the available ones</p>
+
+<button type="button" onclick="document.write(5 + 6)">CLICK HERE</button>
+
+</body>
+</html> 
